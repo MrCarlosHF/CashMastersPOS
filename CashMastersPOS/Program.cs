98a6276456be6eb754ed1cee191ad5f238c94222
Carlos Refugio Hernández Flores <carlos.hernandezf@contpaqi.com>
2021-07-03 20:33:11 -0500

    initial check in

---
+++ CashMastersPOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CashMastersPOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
